d1e83cba2d6b9ea9d6dce3b541314444b4dddcd9
Tim Sotirhos <tim.sotirhos@gmail.com>
2020-01-15 10:59:44 -0600

    inital push

---
+++ tim_capstone_folder/model.py
@@ -0,0 +1,9 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+def split_my_data(df):
+    X = df.drop(columns = ['early_failure', 'failure'])
+    y = df[['early_failure']]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = .80, random_state = 123, stratify=df.early_failure)
+    return X_train, X_test, y_train, y_test
