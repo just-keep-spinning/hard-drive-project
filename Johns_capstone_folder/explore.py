c89e55148c497103731948361b01d687db409d7e
John Salas <johndsalas@gmail.com>
2020-01-15 16:19:22 -0600

    commit

--- Johns_capstone_folder/explore.py
+++ Johns_capstone_folder/explore.py
@@ -1,11 +1,13 @@
-def early_failure(df,cut_off=1.6):
+def apply_cutoff(df,cut_off=2):
     '''
-    Add column to identify early failures based on the age of the hard drive 
+    Apply cutoff date to the dataframe 
+    '''
 
-    '''
-    
+    # remove the drives that did not fail and that are below the cut off age from the data frame
+    df = df[(df.failure==1)|(df.drive_age_in_years > cut_off)]
+    # mark remaining units as early failure (1) or not early failure (2)
     df['early_failure']= 1
-    df['early_failure'][df.drive_age_in_years<=cut_off] = 1
+    df['early_failure'][df.drive_age_in_years>=cut_off] = 1
     df['early_failure'][df.drive_age_in_years>cut_off] = 0
     
     return df
